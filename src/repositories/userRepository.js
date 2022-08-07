2df5696ebfa615f7f557fb773c033ca226fbe343
Victor <victorgpj25@gmail.com>
2022-08-07 19:31:14 -0300

    getUsers route fix 3

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -3,9 +3,9 @@ import connection from "../databases/postgres.js"
 
 async function getUserDataQuery (userId) {
     return connection.query(
-        `SELECT users.id, users.name, SUM(urls."visitCount") as "visitCount" 
+        `SELECT users.id, users.name, COALESCE(SUM(urls."visitCount"), 0) as "visitCount" 
         FROM users 
-        JOIN urls ON urls."userId" = users.id 
+        LEFT JOIN urls ON urls."userId" = users.id 
         WHERE users.id=$1 
         GROUP BY users.id, users.name`,
         [userId]
