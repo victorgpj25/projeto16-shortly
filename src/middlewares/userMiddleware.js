e310dbcb279e6412bdfc5536b3a2e40b3f6c1290
Victor <victorgpj25@gmail.com>
2022-08-07 19:21:42 -0300

    getUsers route fix

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -4,7 +4,7 @@ import {userRepository} from "../repositories/userRepository.js"
 export async function getUserDataMiddleware (req, res, next) {
     const userId = res.locals.userId
 
-    const {rows: userExists} = await userRepository.getUserById(userId)
+    const {rows: userExists} = await userRepository.getUserDataQuery(userId)
 
     if (!userExists.length) {
         return res.sendStatus(404)
